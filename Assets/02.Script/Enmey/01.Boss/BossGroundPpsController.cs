c379a0bf37caf674c5db0cff2725a6665383c1bf
Si Yeong, Jang <51752244+limrum09@users.noreply.github.com>
2025-01-10 02:00:27 +0900

    boss effect

---
+++ Assets/02.Script/Enmey/01.Boss/BossGroundPpsController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGroundPpsController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
